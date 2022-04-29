51634de8e1bbf9f268384e5869129f786f895f58
HyeongYun <guddbs5545@naver.com>
2022-04-29 22:22:09 +0900

    handle state change on editor

--- src/components/editor/editor.jsx
+++ src/components/editor/editor.jsx
@@ -2,12 +2,18 @@ import React from "react";
 import CardEditForm from "../card_edit_form/card_edit_form";
 import styles from "./editor.module.css";
 import CardAddForm from "../card_add_form/card_add_form";
-const Editor = ({ cards, addCard }) => {
+
+const Editor = ({ cards, addCard, updateCard, deleteCard }) => {
   return (
     <section className={styles.editor}>
       <h1 className={styles.title}>Card Maker</h1>
-      {cards.map((card) => (
-        <CardEditForm key={card.id} card={card} />
+      {Object.keys(cards).map((key) => (
+        <CardEditForm
+          key={key}
+          updateCard={updateCard}
+          deleteCard={deleteCard}
+          card={cards[key]}
+        />
       ))}
       <CardAddForm onAdd={addCard}></CardAddForm>
     </section>
--- src/components/preview/preview.jsx
+++ src/components/preview/preview.jsx
@@ -3,13 +3,12 @@ import Card from "../card/card";
 import styles from "./preview.module.css";
 
 const Preview = ({ cards }) => {
-  console.log(cards);
   return (
     <section className={styles.preview}>
       <h1 className={styles.title}>Preview</h1>
       <ul className={styles.cards}>
-        {cards.map((card) => (
-          <Card card={card}></Card>
+        {Object.keys(cards).map((key) => (
+          <Card key={key} card={cards[key]}></Card>
         ))}
       </ul>
     </section>
